e6502d8d698836433e23a0566551fde60a47c7b2
Victor Eduardo Bazterra <bazterra@gmail.com>
2017-09-26 07:57:01 -0400

    Adding closure by copy example.

---
+++ idioms/javascript/ClosureByCopy.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+"use strict";
+
+/**
+ * Create closure forcing capture by copy or value
+ * @param value - Value of context
+ */
+function CreateClosure(value) {
+  const context = JSON.parse(
+    JSON.stringify(value)
+  );
+  function Method() {
+    return context;
+  }
+  return Method;
+}
+
+// Creating closure with a unmutable object (a string)
+// This behaves as if context is captured by copy or value
+const unmutable = 'A'
+const closureA = CreateClosure(unmutable);
+console.log('context in closureA: ' + closureA());
+
+// Creating closure with a mutable object (a list)
+// This behaves as if context is captured by reference
+const mutable = ['B'];
+const closureB = CreateClosure(mutable);
+console.log('context in closureB: ' + closureB());
+
+// It is possible to change of the value of the context
+mutable[0] = 'C';
+console.log('context in closureB: ' + closureB());
