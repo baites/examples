47eb3e29a47f558ba6ea98b426fe6f554926b62a
Victor Eduardo Bazterra <bazterra@gmail.com>
2017-09-25 22:11:07 -0400

    Adding closure by copy example.

---
+++ idioms/python/ClosureByCopy.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+import copy
+
+def CreateClosure(value):
+    """Create closure forcing capture by copy or value."""
+    context = copy.deepcopy(value)
+    def Method():
+        return context
+    return Method
+
+# Creating closure with a unmutable object (a string)
+# This behaves as if context is captured by copy or value
+unmutable = 'A'
+closureA = CreateClosure(unmutable)
+print('context in closureA: ', closureA())
+
+# Creating closure with a mutable object (a list)
+# This also behaves as if context is captured by copy or value
+mutable = ['B']
+closureB = CreateClosure(mutable)
+print('context in closureB: ', closureB())
+
+# It is NOT possible to change of the value of the context
+mutable[0] = 'C'
+print('context in closureB: ', closureB())
