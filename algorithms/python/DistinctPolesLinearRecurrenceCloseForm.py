a753c4faed8f5cfb74bbb9f0988a311bed5196da
Victor Eduardo Bazterra <bazterra@gmail.com>
2017-10-12 20:06:31 -0400

    Improving DistinctPolesLinearRecurrenceCloseForm.

--- algorithms/python/DistinctPolesLinearRecurrenceCloseForm.py
+++ algorithms/python/DistinctPolesLinearRecurrenceCloseForm.py
@@ -15,7 +15,7 @@ def CreateP(a,b):
             p[n] -= a[m-1] * b[n-m]
     return np.poly1d(p[::-1])
 
-def CreateQs(a):
+def CreateQ(a):
     q = np.zeros(len(a)+1)
     q[0] = 1
     for i in range(len(a)):
@@ -33,42 +33,80 @@ def GetCloseForm(P,Q):
         c[k] = -r[k] * np.polyval(P, 1/r[k]) / np.polyval(dQ, 1/r[k])
     return c, r
 
-def EvalCloseForm(c, r, n):
+def EvalCloseForm(c, r, n, format='integer'):
     if len(c) != len(r):
         raise ValueError('Coeff and root arrays are not same size')
     f = 0
     for k in range(len(c)):
         f += c[k] * r[k]**n
+    if format == 'integer':
+        f = int(round(f.real))
+    elif format == 'real':
+        f = f.real
     return f
 
+def LinearRecurrenceSequence(name, size, a, b):
+    print('Name: {}\n'.format(name))
+    print('Degree: {}'.format(len(a)))
+    print()
+    print('Recurrence coefficients')
+    print('a: ', a)
+    print('Initial conditions')
+    print('b: ', b)
+    print()
+    P = CreateP(np.array(a),np.array(b))
+    print('Generating function')
+    print('P:')
+    print(P)
+    Q = CreateQ(np.array(a))
+    print('Q:')
+    print(Q)
+    c, r = GetCloseForm(P,Q)
+    print()
+    print('Partial fraction expantion')
+    print('coefficients (c)  : ')
+    print(c)
+    print('roots: (eta) : ')
+    print(r)
+    print()
+    print('Sequence ({})'.format(size))
+    for n in range(size):
+        print(EvalCloseForm(c, r, n), end=', ')
+    print()
+    print()
+    print('------------------------')
+    print()
 
-# Fibonacchi sequence
-# b = np.array([0,1])
-# a = np.array([1,1])
-# Tribonacci A000073 in the OEIS
-b = np.array([0,0,1])
-a = np.array([1,1,1])
-# Tetranacci A000078 in the OEIS
-# b = np.array([0,0,0,1])
-# a = np.array([1,1,1,1])
-# Pentanacci A001591 in the OEIS
-# b = np.array([0,0,0,0,1])
-# a = np.array([1,1,1,1,1])
-# (P,Q)-Fibonacchi sequence
-# P = 1
-# Q = 2
-# b = np.array([0,1])
-# a = np.array([P,Q])
-# (P,Q)-Lucas sequence of second kind
-# P = 2
-# Q = 1
-# b = np.array([2,P])
-# a = np.array([P,Q])
+# Fibonacchi (A000045)
+b = [0,1]
+a = [1,1]
+LinearRecurrenceSequence('Fibonacchi (A000045)', 20, a, b)
 
-P = CreateP(a,b)
-Q = CreateQs(a)
+# Tribonacci (A000073)
+b = [0,0,1]
+a = [1,1,1]
+LinearRecurrenceSequence('Tribonacci (A000073)', 20, a, b)
 
-c, r = GetCloseForm(P,Q)
-for n in range(20):
-   print(EvalCloseForm(c, r, n),end=', ')
-print()
+# Tetranacci (A000078)
+b = [0,0,0,1]
+a = [1,1,1,1]
+LinearRecurrenceSequence('Tetranacci (A000078)', 20, a, b)
+
+# Pentanacci (A001591)
+b = [0,0,0,0,1]
+a = [1,1,1,1,1]
+LinearRecurrenceSequence('Pentanacci (A001591)', 20, a, b)
+
+# (P,Q)-Fibonacchi
+P = 1
+Q = 2
+b = [0,1]
+a = [P,Q]
+LinearRecurrenceSequence('(1,2)-Fibonacchi: Jacobsthal (A001045)', 20, a, b)
+
+# (P,Q)-Lucas of second kind
+P = 2
+Q = 1
+b = [2,P]
+a = [P,Q]
+LinearRecurrenceSequence('(2,1)-Lucas: Companion Pell (A002203)', 20, a, b)
