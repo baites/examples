ebd7f33c1b96f2b7b2f3cbb089e09085d6069bd5
Victor Eduardo Bazterra <bazterra@gmail.com>
2019-03-27 22:53:16 -0400

    Working in new solution.

--- coding/codeforces/nastya-hasnt-written-a-legend/solution_v1.py
+++ coding/codeforces/nastya-hasnt-written-a-legend/solution_v1.py
@@ -1,209 +1,196 @@
 # python3
 
+from pyds.AVLBinarySearchTree import AVLBinarySearchTreeNode
+from pyds.AVLBinarySearchTree import AVLBinarySearchTree
 from sys import stdin
 
-# Vertex of a splay tree
 
-class Vertex:
-  def __init__(self, key, a, k):
-    self.key = key
-    self.sum = a
-    self.a = a
-    self.k = k
-    self.left = None
-    self.right = None
-    self.parent = None
+class Node(AVLBinarySearchTreeNode):
+    """Implement avl node for implementing an interval tree."""
 
-# Splay tree implementation
+    def __init__(self, key, high, a):
+        super().__init__(key)
+        self.high = high
+        self.maxhigh = high
+        self.a = a
+        self.asum = a
+
+    def label(self):
+        """Customize node label."""
+        return '[{},{}]|{},{}'.format(self.key, self.high, self.a, self.asum)
+
+    def update(self):
+        """Customize node update the keysum."""
+        # Call parent update first
+        super().update()
+        self.maxhigh = self.high
+        self.asum = self.a * (self.high-self.key+1)
+        if self.left is not None:
+            left = self.left
+            self.maxhigh = max(self.maxhigh, left.maxhigh)
+            self.asum += left.asum + left.a * (left.high-left.key)
+        if self.right is not None:
+            right = self.right
+            self.maxhigh = max(self.maxhigh, right.maxhigh)
+            self.asum += right.asum + right.a * (right.high-right.key)
+
+    def overlap(self, point):
+        """Check if point overlaps with self."""
+        if self.high < point or point < self.key:
+            return False
+        return True
 
 
-def update(v):
-  if v == None:
-    return
-  v.sum = v.a + (v.left.sum if v.left != None else 0) + (v.right.sum if v.right != None else 0)
-  if v.left != None:
-    v.left.parent = v
-  if v.right != None:
-    v.right.parent = v
+class IntervalTree(AVLBinarySearchTree):
+    """Implement interval tree based on AVL."""
 
-def smallRotation(v):
-  parent = v.parent
-  if parent == None:
-    return
-  grandparent = v.parent.parent
-  if parent.left == v:
-    m = v.right
-    v.right = parent
-    parent.left = m
-  else:
-    m = v.left
-    v.left = parent
-    parent.right = m
-  update(parent)
-  update(v)
-  v.parent = grandparent
-  if grandparent != None:
-    if grandparent.left == parent:
-      grandparent.left = v
+    def __init__(self, root=None):
+        """Constructor."""
+        super().__init__(root)
+
+    def interval(self, point):
+        """Return a node that overlap with point."""
+        node = self._root
+        while node is not None \
+            and not node.overlap(point):
+            if node.left is not None \
+                and node.left.maxhigh >= point:
+                node = node.left
+            else:
+                node = node.right
+        return node
+
+    def end_interval(self, a):
+        """Search last interval to be updated."""
+        node = self._root
+        while 1:
+            if a > node.a:
+                if node.right is None:
+                    break
+                node = node.right
+            elif a < node.a:
+                if node.left is None:
+                    break
+                node = node.left
+            else:
+                break
+        if a < node.a:
+            return node.prev()
+        return node
+
+
+def CreateTree(a, k):
+    """Create a tree of intervals."""
+    # Initially all the intervals are size 1
+    n = len(a)
+    tree = IntervalTree()
+    for i in range(len(a)):
+        vk = k[i] if i < n-1 else 0
+        tree.insert(Node(i+1, i+1, a[i]))
+    return tree
+
+
+def GetSum(ltree, i, j):
+    """Compute the sum between elements based on intervals."""
+
+    # Search for interal where index i is present O(log N)
+    start = ltree.interval(i);
+    # Search for interal where index j is present O(log N)
+    end = ltree.interval(j);
+
+    # Case if start and end intervals are the same
+    if start.key == end.key:
+        return start.a * (j-i+1)
+
+    # Case if start and end are different
+    # Split the tree in three section
+    if start.key == 1:
+        mtree = ltree
     else:
-      grandparent.right = v
+        mtree = ltree.split(start.key)
+    rtree = mtree.split(end.key)
 
-def bigRotation(v):
-  if v.parent.left == v and v.parent.parent.left == v.parent:
-    # Zig-zig
-    smallRotation(v.parent)
-    smallRotation(v)
-  elif v.parent.right == v and v.parent.parent.right == v.parent:
-    # Zig-zig
-    smallRotation(v.parent)
-    smallRotation(v)
-  else:
-    # Zig-zag
-    smallRotation(v)
-    smallRotation(v)
+    # The middle tree contains all the intervals
+    # from the next to start until end interval
+    # So the root of the middle tree has the sum
+    # except for contribution from start node.
+    asum = mtree._root.asum
 
-# Makes splay of the given vertex and makes
-# it the new root.
-def splay(v):
-  if v == None:
-    return None
-  while v.parent != None:
-    if v.parent.parent == None:
-      smallRotation(v)
-      break
-    bigRotation(v)
-  return v
+    # Rebuild the tree
+    mtree.merge(rtree)
+    if start.key != 1:
+        ltree.merge(mtree)
 
-# Searches for the given key in the tree with the given root
-# and calls splay for the deepest visited node after that.
-# Returns pair of the result and the new root.
-# If found, result is a pointer to the node with the given key.
-# Otherwise, result is a pointer to the node with the smallest
-# bigger key (next value in the order).
-# If the key is bigger than all keys in the tree,
-# then result is None.
-def find(root, key):
-  v = root
-  last = root
-  next = None
-  while v != None:
-    if v.key >= key and (next == None or v.key < next.key):
-      next = v
-    last = v
-    if v.key == key:
-      break
-    if v.key < key:
-      v = v.right
-    else:
-      v = v.left
-  root = splay(last)
-  return (next, root)
+    # Return the sum after adding start interval contribution
+    return asum + start.a * (start.high-i+1)
 
 
-def split(root, key):
-  (result, root) = find(root, key)
-  if result == None:
-    return (root, None)
-  right = splay(result)
-  left = right.left
-  right.left = None
-  if left != None:
-    left.parent = None
-  update(left)
-  update(right)
-  return (left, right)
+def Add(tree, i, x):
+    """Add a value and recompute intervals if needed."""
+
+    # Edge case
+    if x == 0:
+        return
+
+    # Search interval overlaping with index i O(log N)
+    start = tree.interval(i)
+
+    # Compute the new value of a
+    a = start.a + x
+
+    # Search end interval to update O(log N)
+    end = tree.end_interval(a)
+
+    # No need for propagate the update
+    # if start and end intervals are the same O(log N)
+    if start.key == end.key:
+        node = tree.delete(i)
+        node.a = a
+        tree.insert(node)
+        return
+
+    # Remove all the intervals in the middle
+    # by splitting the tree O(?)
+    mtree = tree.split(start.key)
+    tree.delete(start.key)
+    rtree = mtree.split(end.key)
+
+    # Create a new interval covering
+    # all the updated values O(log N)
+    node = Node(start.key, end.key, a)
+    # Insert the node in the left tree
+    tree.insert(node)
+    # Merge the right tree discarding the middle
+    tree.merge(rtree)
 
 
-def merge(left, right):
-  if left == None:
-    return right
-  if right == None:
-    return left
-  while right.left != None:
-    right = right.left
-  right = splay(right)
-  right.left = left
-  update(right)
-  return right
+def main():
 
-# Code that uses splay tree to solve the problem
+    n = int(input().strip())
+    a = input().strip().split()
+    a = [int(a[i]) for i in range(n)]
+    k = input().strip().split()
+    k = [int(k[i]) for i in range(n-1)]
+    q = int(input().strip())
 
-root = None
+    tree = CreateTree(a, k)
 
-def insert(key, a, k):
-  global root
-  (left, right) = split(root, key)
-  new_vertex = None
-  if right == None or right.key != key:
-    new_vertex = Vertex(key, a, k)
-  root = merge(merge(left, new_vertex), right)
+    for i in range(q):
+        print(tree)
+        print()
+        line = input().split()
+        if line[0] == 's':
+            l = int(line[1])
+            r = int(line[2])
+            print(GetSum(tree, l, r))
+        elif line[0] == '+':
+            i = int(line[1])
+            x = int(line[2])
+            Add(tree, i, x)
+        else:
+            print('unknown ops')
 
-def erase(x):
-  global root
-  (left, right) = split(root, x)
-  if right == None or right.key != x:
-    root = merge(left, right)
-    return
-  old_node = right
-  middle = old_node.left
-  if middle:
-    middle.parent = None
-  right = old_node.right
-  if right:
-    right.parent = None
-  del old_node
-  root = merge(left, right)
+    print(tree)
 
-def search(x):
-  global root
-  result, root = find(root, x)
-  if result == None or result.key != x:
-      return False
-  return True
-
-def sum(fr, to):
-  global root
-  (left, middle) = split(root, fr)
-  (middle, right) = split(middle, to + 1)
-  val = middle.sum if middle else 0
-  root = merge(left, merge(middle, right))
-  return val
-
-
-def add_value(i, x):
-  global root
-  node, root = find(root, i)
-  node.a += x
-  update(node)
-  next, root = find(root, i+1)
-  if next is not None and next.a < node.a + node.k:
-    add_value(i+1, node.a + node.k - next.a)
-
-
-# Main program
-
-n = int(input().strip())
-a = input().strip().split()
-a = [int(a[i]) for i in range(n)]
-k = input().strip().split()
-k = [int(k[i]) for i in range(n-1)]
-q = int(input().strip())
-
-for i in range(n):
-    vk = k[i] if i < n-1 else 0
-    insert(i+1, a[i], vk)
-
-for i in range(q):
-    line = input().split()
-    if line[0] == 's':
-        l = int(line[1])
-        r = int(line[2])
-        result = sum(l, r)
-        print(result)
-    elif line[0] == '+':
-        i = int(line[1])
-        x = int(line[2])
-        node, root = find(root, i)
-        add_value(i, x)
-    else:
-        print('unknown ops')
+if __name__ == "__main__":
+    main()
