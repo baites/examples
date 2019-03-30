bee11d2c79f912d767f50149f4de0eef6245eee3
Victor Eduardo Bazterra <bazterra@gmail.com>
2019-03-29 20:53:42 -0400

    Final changes using interval tree.

--- coding/codeforces/nastya-hasnt-written-a-legend/instance-generator.py
+++ coding/codeforces/nastya-hasnt-written-a-legend/instance-generator.py
@@ -1,19 +1,25 @@
 
 import random
 
-ninsts = 10
-maxsize = 40
-maxops = 30
-maxv = 20
+mininst = 44
+maxinst = 45
+maxsize = 1000
+maxops = 10000
+maxv = 400
 minv = -maxv
-maxx = 20
-psum = 0.5
+maxx = 100
+psum = 0.1
 
 dir = 'kzeros_tests'
 
-for n in range(0, ninsts):
+for n in range(mininst, maxinst):
     size = random.randint(2, maxsize)
+    size = 40000
+    macv = 1000000000
+    maxv =  1000000000
+    minv = -1000000000
     a = [random.randint(minv,maxv) for i in range(size)]
+    #a = [1 for i in range(size)]
     a.sort()
     a = [str(v) for v in a]
     with open('{}/{:04d}'.format(dir, n), 'w') as f:
@@ -28,8 +34,8 @@ for n in range(0, ninsts):
                     i, x
                 ), file=f)
             else:
-                l = random.randint(1, size)
-                r = random.randint(l, size)
+                l = random.randint(1, size//2)
+                r = random.randint(size//2+1, size)
                 print('s {} {}'.format(
                     l, r
                 ), file=f)
